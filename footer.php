22c5b8d913636a8dbad25023efe8bdcc3a50d931
rachelorban <53211994+rachelorban@users.noreply.github.com>
2020-11-20 15:38:19 +0100

    Footer
    Footer

--- footer.php
+++ footer.php
@@ -1,8 +1,13 @@
 </div><!-- /.row -->
 
 </div><!-- /.container -->
+
+
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+                <path fill="#4B9228" fill-opacity="1" d="M0,288L80,261.3C160,235,320,181,480,181.3C640,181,800,235,960,245.3C1120,256,1280,224,1360,208L1440,192L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z" data-darkreader-inline-fill="" style="--darkreader-inline-fill:#4B9228;"></path>
+            </svg>
 <div class="footer">
-            
+        
             <div class="container-fluid">
                 <div class="row">
                     <div class="col ">
